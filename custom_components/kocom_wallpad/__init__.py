6f6421a0f937d51071fb1e89ca4ca60645247867
zmtq05 <zmtq05@gmail.com>
2024-05-30 12:19:25 +0000

    WIP: basic light entry

--- custom_components/kocom_wallpad/__init__.py
+++ custom_components/kocom_wallpad/__init__.py
@@ -1,13 +1,26 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
+from .ew11 import Ew11
+
 from .const import DOMAIN
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = ew11 = Ew11(hass, entry)
+    await ew11.async_connect()
+    entry.async_create_background_task(hass, ew11.listen(), "listen")
+
+    await hass.config_entries.async_forward_entry_setup(entry, "light")
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return True
+    if unload_ok := await hass.config_entries.async_forward_entry_unload(
+        entry, "light"
+    ):
+        ew11 = hass.data[DOMAIN].pop(entry.entry_id)
+        await ew11.async_disconnect()
+
+    return unload_ok
---
+++ custom_components/kocom_wallpad/util.py
@@ -0,0 +1,16 @@
+from typing import TypedDict, cast
+from homeassistant.config_entries import ConfigEntry
+
+
+class EntryData(TypedDict):
+    host: str
+    port: int
+    light: dict[str, int]
+    thermostat: dict[str, bool]
+    fan: bool
+    gas: bool
+    elevator: bool
+
+
+def get_data(entry: ConfigEntry) -> EntryData:
+    return cast(EntryData, entry.data)
