6aac9427d0f2a7d0d80a642b27eecf6dfb7d1d56
zmtq05 <zmtq05@gmail.com>
2024-06-28 14:05:31 +0000

    fix: already loaded issue

--- custom_components/kocom_wallpad/__init__.py
+++ custom_components/kocom_wallpad/__init__.py
@@ -1,9 +1,19 @@
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
 from homeassistant.config_entries import ConfigEntry
+
+from custom_components.kocom_wallpad.util import typed_data
 
 from .ew11 import Ew11
 
 from .const import DOMAIN
+
+PLATFORMS: list[Platform] = [
+    Platform.LIGHT,
+    Platform.CLIMATE,
+    Platform.FAN,
+    Platform.VALVE,
+]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
@@ -13,16 +23,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     entry.async_create_background_task(hass, ew11.read_loop(), "read_loop")
     entry.async_create_background_task(hass, ew11.send_loop(), "send_loop")
 
-    await hass.config_entries.async_forward_entry_setups(
-        entry, ["light", "climate", "fan", "valve"]
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    if unload_ok := await hass.config_entries.async_forward_entry_unload(
-        entry, "light"
-    ):
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         ew11: Ew11 = hass.data[DOMAIN].pop(entry.entry_id)
         await ew11.disconnect()
 
