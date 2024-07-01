00b796e2ec5f541a0ecb6b727e0612e2847e7077
zmtq05 <zmtq05@gmail.com>
2024-07-01 13:54:27 +0000

    refactor

--- custom_components/kocom_wallpad/light.py
+++ custom_components/kocom_wallpad/light.py
@@ -9,8 +9,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .hub import Hub, LightController
-from .util import typed_data
-from .const import CONF_LIGHT, DOMAIN
+from .const import DOMAIN
 
 
 async def async_setup_entry(
@@ -18,13 +17,9 @@ async def async_setup_entry(
 ):
     """Set up the Kocom Wallpad light entity."""
     hub: Hub = hass.data[DOMAIN][entry.entry_id]
-    data = typed_data(entry)
-    for room, light_size in data[CONF_LIGHT].items():
-        room = int(room)
-        controller = hub.light_controllers[room]
-        entry.async_create_task(hass, controller.refresh())
+    for controller in hub.light_controllers.values():
         async_add_entities(
-            [KocomLightEntity(room, light, controller) for light in range(light_size)]
+            [KocomLightEntity(controller, n) for n in range(controller.size)]
         )
 
 
@@ -38,32 +33,31 @@ class KocomLightEntity(LightEntity):
 
     def __init__(
         self,
-        room: int,
-        light: int,
         controller: LightController,
+        n: int,
     ):
         """Initialize the Kocom Light entity."""
-        self.room = room
-        self.light = light
+        self.n = n
         self.controller = controller
-        self._attr_name = f"방{room} 조명{light+1}"
-        self._attr_unique_id = f"room_{room}_light_{light+1}"
+        self._attr_name = f"조명{controller.room}-{n}"
+        self._attr_unique_id = f"light_{controller.room}_{n}"
 
     @property
     def is_on(self) -> bool:
         """Return true if the light is on."""
-        return self.controller.is_on(self.light)
+        return self.controller.is_on(self.n)
 
     async def async_turn_on(self) -> None:
         """Turn on the light."""
-        await self.controller.turn_on(self.light)
+        await self.controller.turn_on(self.n)
 
     async def async_turn_off(self) -> None:
         """Turn off the light."""
-        await self.controller.turn_off(self.light)
+        await self.controller.turn_off(self.n)
 
     async def async_added_to_hass(self) -> None:
         """Register the callback."""
+        await self.controller.refresh()
         self.controller.register_callback(self.async_write_ha_state)
 
     async def async_will_remove_from_hass(self) -> None:
